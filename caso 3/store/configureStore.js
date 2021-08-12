fa067227ec6d0baad2329a92c953e4ba5e74a3d1
vanderL <vnd.vander@gmail.com>
2021-08-12 17:24:42 -0300

    Organização

---
+++ caso 3/store/configureStore.js
@@ -0,0 +1,7 @@
+import contador from './contador.js';
+
+const reducer = Redux.combineReducers({contador});
+
+const store = Redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
---
+++ caso 3/store/contador.js
@@ -0,0 +1,25 @@
+const INCREMENTAR = 'contador/INCREMENTAR';
+const REDUZIR = 'contador/REDUZIR';
+
+export const incrementar = () => ({
+    type: INCREMENTAR
+})
+
+export const reduzir = () => ({
+    type: REDUZIR
+})
+
+const initialState = 0;
+
+const contador = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENTAR:
+            return state + 1;
+        case REDUZIR:
+            return state - 1;
+        default:
+            return state
+    }
+}
+
+export default contador
