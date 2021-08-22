2d4db66229a899c73cb9a5d99d249ed1cf879781
vanderL <vnd.vander@gmail.com>
2021-08-21 23:26:39 -0300

    Dispatchs Async

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { incrementar, reduzir } from './store/contador';
-import { abrir, fechar } from './store/modal';
+import { login } from './store/login';
 
 
 const App = () => {
-  const state = useSelector(state => state);
-  console.log(state)
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
   const dispatch = useDispatch()
+  const {data} = useSelector(state => state.login.user)
+  //console.log(token)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(login({username, password}))
+
+  }
 
   return (
     <div>
-      {state.modal && <h1>Total: {state.contador.total}</h1>}
-      
-      <button onClick={() => dispatch(incrementar())}> Incrementar </button>
-      <button onClick={() => dispatch(reduzir())}> Reduzir </button>
-      <button onClick={() => dispatch(abrir())}> Abrir Modal </button>
-      <button onClick={() => dispatch(fechar())}> Fechar Modal </button>
+      <form onSubmit={handleSubmit}>
+        <label style={{ display: 'block'}} htmlFor="username">
+          Usuário
+        </label>
+        <input 
+          id="username"
+          type="text" 
+          value={username}
+          onChange={({target}) => setUsername(target.value)}
+        />
+
+        <label style={{ display: 'block'}} htmlFor="password">
+          Senha</label>
+        <input 
+          id="password" 
+          type="password" 
+          value={password}
+          onChange={({target}) => setPassword(target.value)}
+        />
+
+        <button> {data.loading ? 'carregando' : 'Enviar'} </button>
+        <p>{data?.id}</p>
+       
+     </form>
     </div>
   );
 }
